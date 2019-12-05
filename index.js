a33b3028d716c25c156776f6e6db2340bef28dc2
Keith Bonarrigo <keith@keithbonline.com>
2019-12-05 10:02:11 -0700

    adding handlebars layouts

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const exphbs  = require('express-handlebars');
 
 const PORT = process.env.port || 5000;
+
+//set handlebars middleware
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+ 
+app.get('/', function (req, res) {
+    res.render('home');
+});
 
 //set static folder
 app.use(express.static(path.join(__dirname, 'public')));
